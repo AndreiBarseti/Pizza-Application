9b45c6ddf2fd6ac78c6929b77c490a71c189d2fe
AndreiBarseti <andrei.barseti@bytex.ro>
2021-09-07 16:37:35 +0300

    src for cr

---
+++ src/app/pizza.ts
@@ -0,0 +1,16 @@
+export interface Pizza {
+    id: number,
+    name: string;
+    image_url: string;
+    topping: string;
+    popularity: string;
+    description: string;
+    price: number;
+    alergeni: string;
+    ingredients: string;
+    quantity: number;
+    total: string;
+}
+//pam-pam
+//maybe it works this time
+///haaai
